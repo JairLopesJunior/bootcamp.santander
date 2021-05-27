e8c93dc5e28e9af73493dff7c4b83c88f0f20aaf
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-05-27 17:22:06 -0300

    Criado as Classes de Controller com os metodos save(), update(), finAll() e findById(). E o DTO da nossa Entity

---
+++ src/main/java/com/project/bootcamp/santander/controller/StockController.java
@@ -0,0 +1,39 @@
+package com.project.bootcamp.santander.controller;
+
+import com.project.bootcamp.santander.model.dto.StockDTO;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/stock")
+public class StockController {
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<StockDTO> save(@RequestBody @Valid StockDTO dto){
+        return ResponseEntity.ok(dto);
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<StockDTO> update(@RequestBody @Valid StockDTO dto){
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<StockDTO>> findAll(){
+        List<StockDTO> list = new ArrayList<>();
+        StockDTO dto = new StockDTO();
+        dto.setId(1L);
+        dto.setName("Magazine Luiza");
+        dto.setPrice(100.0);
+        dto.setVariation(1.00);
+        dto.setDate(LocalDate.now());
+        list.add(dto);
+        return ResponseEntity.ok(list);
+    }
+}
